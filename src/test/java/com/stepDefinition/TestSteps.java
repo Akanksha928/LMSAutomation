c3f26dbb40ca34ddad701b1bb7924416a2fa658a
Akanksha Sharan Basappa <akanksha.sharan09@gmail.com>
2022-12-09 12:56:29 +0530

    PageFactory implementation

--- src/test/java/com/stepDefinition/TestSteps.java
+++ src/test/java/com/stepDefinition/TestSteps.java
@@ -6,10 +6,10 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.PageFactory;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
@@ -21,6 +21,9 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import pages.ClickOnHolidayList;
+import pages.DisplayHolidays;
+import pages.ValidateHolidays;
 
 public class TestSteps {
 
@@ -31,18 +34,25 @@ public class TestSteps {
 
 	@Given("User is on EY LMS page")
 	public void user_is_on_ey_lms_page() {
-		System.setProperty("webdriver.edge.driver", "C:\\bb\\drivers\\edgedriver_win64\\msedgedriver.exe");
-		driver = new EdgeDriver();
 
+		// Setting up
+		System.setProperty("webdriver.edge.driver", "Drivers\\msedgedriver.exe");
+		driver = new EdgeDriver();
 		driver.get("https://lms.ey.net/");
 		driver.manage().window().maximize();
 
-		PropertyConfigurator
-				.configure("C:\\Users\\FU276CJ\\eclipse-workspace\\lms-ey\\src\\test\\resources\\log4j.properties");
+		// Configuring log4j
+		PropertyConfigurator.configure("src\\test\\resources\\log4j.properties");
+
 		report = new ExtentReports();
-		ExtentSparkReporter spark = new ExtentSparkReporter("target/spark.html");
+		report.setSystemInfo("OS", System.getProperty("os.name"));
+		report.setSystemInfo("Java Version", System.getProperty("java.version"));
+		report.setSystemInfo("URL", "https://lms.ey.net/");
+
+		ExtentSparkReporter spark = new ExtentSparkReporter("target/report.html");
 		report.attachReporter(spark);
 		test = report.createTest("HolidaysList");
+
 		test.log(Status.PASS, "LMS Webpage opened");
 		log.info("LMS Webpage opened");
 
@@ -50,9 +60,8 @@ public class TestSteps {
 
 	@When("User navigates to the Holiday List Page")
 	public void user_navigates_to_the_holiday_list_page() {
-		driver.findElement(By.cssSelector(
-				"#wrap > div.container-fluid.nonresponsive > div:nth-child(2) > div.row > div.col-sm-7 > div:nth-child(2) > div:nth-child(1) > div > div.row > div:nth-child(1) > a"))
-				.click();
+		ClickOnHolidayList display = PageFactory.initElements(driver, ClickOnHolidayList.class);
+		display.clickOnHolidaysLink();
 		test.log(Status.PASS, "Clicked on Holidays List link");
 		log.info("Clicked on Holidays link");
 	}
@@ -60,15 +69,14 @@ public class TestSteps {
 	@Then("Validate if the public holiday count is equal to or greater than {int}")
 	public void validate_if_the_public_holiday_count_is_equal_to_or_greater_than(Integer int1) {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-		List<WebElement> s = driver.findElements(By.xpath("//*[contains(text(),'Public Holiday')]"));
+		ValidateHolidays validation = PageFactory.initElements(driver, ValidateHolidays.class);
+
 		log.info("Checking if public holidays are greater or lesser than 10");
-		if (s.size() >= 10) {
-			System.out.println("Public Holidays are greater than or equal to 10");
+		if (validation.checkSize() >= 10) {
 			test.fail("Public holidays are greater than or equal to 10");
 			log.info("Public holidays less than or equal to 10");
 
 		} else {
-			System.out.println("Public Holidays are less than 10");
 			test.pass("Public holidays less than 10");
 			log.info("Public holidays less than 10");
 		}
@@ -77,13 +85,13 @@ public class TestSteps {
 
 	@Then("User is able to split the holiday details as per Holiday Type")
 	public void user_is_able_to_split_the_holiday_details_as_per_holiday_type() {
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
-		List<WebElement> Holidays = driver.findElements(By.tagName("tr"));
+		DisplayHolidays display = PageFactory.initElements(driver, DisplayHolidays.class);
 
 		List<String> optionalHols = new ArrayList<String>();
 
-		for (WebElement wb : Holidays) {
+		// Iterating through list of web elements to retrieve optional holidays
+		for (WebElement wb : display.retrieveHols()) {
 			if (wb.getText().contains("Optional Holiday")) {
 				optionalHols.add(wb.getText().replace("Optional Holiday", ""));
 			}
@@ -95,7 +103,8 @@ public class TestSteps {
 
 		List<String> publicHols = new ArrayList<String>();
 
-		for (WebElement wb : Holidays) {
+
+		for (WebElement wb : display.retrieveHols()) {
 			if (wb.getText().contains("Public Holiday")) {
 				publicHols.add(wb.getText().replace("Public Holiday", ""));
 			}
@@ -105,8 +114,8 @@ public class TestSteps {
 		test.info(MarkupHelper.createOrderedList(publicHols));
 		log.info("Printed list of Public holidays in report");
 		report.flush();
-		driver.quit();
 		log.info("Browser closed.");
+		driver.quit();
 
 	}
 
